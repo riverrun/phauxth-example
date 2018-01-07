cb56a2877e3136416d8e4b019ca01f4c1c0d284e
David Whitlock <alovedalongthe@gmail.com>
2018-01-07 17:51:36 +0700

    Updated tests

--- test/forks_the_egg_sample_web/controllers/session_controller_test.exs
+++ test/forks_the_egg_sample_web/controllers/session_controller_test.exs
@@ -17,53 +17,59 @@ defmodule ForksTheEggSampleWeb.SessionControllerTest do
     {:ok, %{conn: conn, user: user}}
   end
 
-  test "rendering login form fails for user that is already logged in", %{conn: conn, user: user} do
-    conn = conn |> add_phauxth_session(user) |> send_resp(:ok, "/")
-    conn = get(conn, session_path(conn, :new))
-    assert redirected_to(conn) == page_path(conn, :index)
+  describe "login form" do
+    test "rendering login form fails for user that is already logged in", %{conn: conn, user: user} do
+      conn = conn |> add_phauxth_session(user) |> send_resp(:ok, "/")
+      conn = get(conn, session_path(conn, :new))
+      assert redirected_to(conn) == page_path(conn, :index)
+    end
   end
 
-  test "login succeeds", %{conn: conn} do
-    conn = post(conn, session_path(conn, :create), session: @create_attrs)
-    assert redirected_to(conn) == user_path(conn, :index)
+  describe "create session" do
+    test "login succeeds", %{conn: conn} do
+      conn = post(conn, session_path(conn, :create), session: @create_attrs)
+      assert redirected_to(conn) == user_path(conn, :index)
+    end
+
+    test "login fails for user that is not yet confirmed", %{conn: conn} do
+      conn = post(conn, session_path(conn, :create), session: @unconfirmed_attrs)
+      assert redirected_to(conn) == session_path(conn, :new)
+    end
+
+    test "login fails for user that is already logged in", %{conn: conn, user: user} do
+      conn = conn |> add_phauxth_session(user) |> send_resp(:ok, "/")
+      conn = post(conn, session_path(conn, :create), session: @create_attrs)
+      assert redirected_to(conn) == page_path(conn, :index)
+    end
+
+    test "login fails for invalid password", %{conn: conn} do
+      conn = post(conn, session_path(conn, :create), session: @invalid_attrs)
+      assert redirected_to(conn) == session_path(conn, :new)
+    end
+
+    test "redirects to previously requested resource", %{conn: conn, user: user} do
+      conn = get(conn, user_path(conn, :show, user))
+      assert redirected_to(conn) == session_path(conn, :new)
+      conn = post(conn, session_path(conn, :create), session: @create_attrs)
+      assert redirected_to(conn) == user_path(conn, :show, user)
+    end
+
+    test "remember me cookie is added / not added", %{conn: conn} do
+      rem_conn = post(conn, session_path(conn, :create), session: @rem_attrs)
+      assert rem_conn.resp_cookies["remember_me"]
+      no_rem_conn = post(conn, session_path(conn, :create), session: @no_rem_attrs)
+      refute no_rem_conn.resp_cookies["remember_me"]
+    end
   end
 
-  test "login fails for user that is not yet confirmed", %{conn: conn} do
-    conn = post(conn, session_path(conn, :create), session: @unconfirmed_attrs)
-    assert redirected_to(conn) == session_path(conn, :new)
-  end
-
-  test "login fails for user that is already logged in", %{conn: conn, user: user} do
-    conn = conn |> add_phauxth_session(user) |> send_resp(:ok, "/")
-    conn = post(conn, session_path(conn, :create), session: @create_attrs)
-    assert redirected_to(conn) == page_path(conn, :index)
-  end
-
-  test "login fails for invalid password", %{conn: conn} do
-    conn = post(conn, session_path(conn, :create), session: @invalid_attrs)
-    assert redirected_to(conn) == session_path(conn, :new)
-  end
-
-  test "logout succeeds and session is deleted", %{conn: conn, user: user} do
-    conn = conn |> add_phauxth_session(user) |> send_resp(:ok, "/")
-    conn = delete(conn, session_path(conn, :delete, user))
-    assert redirected_to(conn) == page_path(conn, :index)
-    conn = get(conn, user_path(conn, :index))
-    assert redirected_to(conn) == session_path(conn, :new)
-    assert Accounts.list_sessions(user.id) == %{}
-  end
-
-  test "redirects to previously requested resource", %{conn: conn, user: user} do
-    conn = get(conn, user_path(conn, :show, user))
-    assert redirected_to(conn) == session_path(conn, :new)
-    conn = post(conn, session_path(conn, :create), session: @create_attrs)
-    assert redirected_to(conn) == user_path(conn, :show, user)
-  end
-
-  test "remember me cookie is added / not added", %{conn: conn} do
-    rem_conn = post(conn, session_path(conn, :create), session: @rem_attrs)
-    assert rem_conn.resp_cookies["remember_me"]
-    no_rem_conn = post(conn, session_path(conn, :create), session: @no_rem_attrs)
-    refute no_rem_conn.resp_cookies["remember_me"]
+  describe "delete session" do
+    test "logout succeeds and session is deleted", %{conn: conn, user: user} do
+      conn = conn |> add_phauxth_session(user) |> send_resp(:ok, "/")
+      conn = delete(conn, session_path(conn, :delete, user))
+      assert redirected_to(conn) == page_path(conn, :index)
+      conn = get(conn, user_path(conn, :index))
+      assert redirected_to(conn) == session_path(conn, :new)
+      assert Accounts.list_sessions(user.id) == %{}
+    end
   end
 end
--- test/forks_the_egg_sample_web/controllers/user_controller_test.exs
+++ test/forks_the_egg_sample_web/controllers/user_controller_test.exs
@@ -21,74 +21,86 @@ defmodule ForksTheEggSampleWeb.UserControllerTest do
     end
   end
 
-  @tag login: "reg@example.com"
-  test "lists all entries on index", %{conn: conn} do
-    conn = get(conn, user_path(conn, :index))
-    assert html_response(conn, 200) =~ "Listing Users"
+  describe "index" do
+    @tag login: "reg@example.com"
+    test "lists all entries on index", %{conn: conn} do
+      conn = get(conn, user_path(conn, :index))
+      assert html_response(conn, 200) =~ "Listing Users"
+    end
+
+    test "renders /users error for unauthorized user", %{conn: conn} do
+      conn = get(conn, user_path(conn, :index))
+      assert redirected_to(conn) == session_path(conn, :new)
+    end
   end
 
-  test "renders /users error for unauthorized user", %{conn: conn} do
-    conn = get(conn, user_path(conn, :index))
-    assert redirected_to(conn) == session_path(conn, :new)
+  describe "renders forms" do
+    test "renders form for new users", %{conn: conn} do
+      conn = get(conn, user_path(conn, :new))
+      assert html_response(conn, 200) =~ "New User"
+    end
+
+    @tag login: "reg@example.com"
+    test "renders form for editing chosen user", %{conn: conn, user: user} do
+      conn = get(conn, user_path(conn, :edit, user))
+      assert html_response(conn, 200) =~ "Edit User"
+    end
   end
 
-  test "renders form for new users", %{conn: conn} do
-    conn = get(conn, user_path(conn, :new))
-    assert html_response(conn, 200) =~ "New User"
+  describe "show user resource" do
+    @tag login: "reg@example.com"
+    test "show chosen user's page", %{conn: conn, user: user} do
+      conn = get(conn, user_path(conn, :show, user))
+      assert html_response(conn, 200) =~ "Show User"
+    end
   end
 
-  @tag login: "reg@example.com"
-  test "show chosen user's page", %{conn: conn, user: user} do
-    conn = get(conn, user_path(conn, :show, user))
-    assert html_response(conn, 200) =~ "Show User"
+  describe "create user" do
+    test "creates user when data is valid", %{conn: conn} do
+      conn = post(conn, user_path(conn, :create), user: @create_attrs)
+      assert redirected_to(conn) == session_path(conn, :new)
+    end
+
+    test "does not create user and renders errors when data is invalid", %{conn: conn} do
+      conn = post(conn, user_path(conn, :create), user: @invalid_attrs)
+      assert html_response(conn, 200) =~ "New User"
+    end
   end
 
-  test "creates user when data is valid", %{conn: conn} do
-    conn = post(conn, user_path(conn, :create), user: @create_attrs)
-    assert redirected_to(conn) == session_path(conn, :new)
+  describe "updates user" do
+    @tag login: "reg@example.com"
+    test "updates chosen user when data is valid", %{conn: conn, user: user} do
+      conn = put(conn, user_path(conn, :update, user), user: @update_attrs)
+      assert redirected_to(conn) == user_path(conn, :show, user)
+      updated_user = Accounts.get(user.id)
+      assert updated_user.email == "william@example.com"
+      conn = get(conn, user_path(conn, :show, user))
+      assert html_response(conn, 200) =~ "william@example.com"
+    end
+
+    @tag login: "reg@example.com"
+    test "does not update chosen user and renders errors when data is invalid", %{
+      conn: conn,
+      user: user
+    } do
+      conn = put(conn, user_path(conn, :update, user), user: @invalid_attrs)
+      assert html_response(conn, 200) =~ "Edit User"
+    end
   end
 
-  test "does not create user and renders errors when data is invalid", %{conn: conn} do
-    conn = post(conn, user_path(conn, :create), user: @invalid_attrs)
-    assert html_response(conn, 200) =~ "New User"
-  end
+  describe "delete user" do
+    @tag login: "reg@example.com"
+    test "deletes chosen user", %{conn: conn, user: user} do
+      conn = delete(conn, user_path(conn, :delete, user))
+      assert redirected_to(conn) == session_path(conn, :new)
+      refute Accounts.get(user.id)
+    end
 
-  @tag login: "reg@example.com"
-  test "renders form for editing chosen user", %{conn: conn, user: user} do
-    conn = get(conn, user_path(conn, :edit, user))
-    assert html_response(conn, 200) =~ "Edit User"
-  end
-
-  @tag login: "reg@example.com"
-  test "updates chosen user when data is valid", %{conn: conn, user: user} do
-    conn = put(conn, user_path(conn, :update, user), user: @update_attrs)
-    assert redirected_to(conn) == user_path(conn, :show, user)
-    updated_user = Accounts.get(user.id)
-    assert updated_user.email == "william@example.com"
-    conn = get(conn, user_path(conn, :show, user))
-    assert html_response(conn, 200) =~ "william@example.com"
-  end
-
-  @tag login: "reg@example.com"
-  test "does not update chosen user and renders errors when data is invalid", %{
-    conn: conn,
-    user: user
-  } do
-    conn = put(conn, user_path(conn, :update, user), user: @invalid_attrs)
-    assert html_response(conn, 200) =~ "Edit User"
-  end
-
-  @tag login: "reg@example.com"
-  test "deletes chosen user", %{conn: conn, user: user} do
-    conn = delete(conn, user_path(conn, :delete, user))
-    assert redirected_to(conn) == session_path(conn, :new)
-    refute Accounts.get(user.id)
-  end
-
-  @tag login: "reg@example.com"
-  test "cannot delete other user", %{conn: conn, other: other} do
-    conn = delete(conn, user_path(conn, :delete, other))
-    assert redirected_to(conn) == user_path(conn, :index)
-    assert Accounts.get(other.id)
+    @tag login: "reg@example.com"
+    test "cannot delete other user", %{conn: conn, other: other} do
+      conn = delete(conn, user_path(conn, :delete, other))
+      assert redirected_to(conn) == user_path(conn, :index)
+      assert Accounts.get(other.id)
+    end
   end
 end
